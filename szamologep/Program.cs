c8dc48017bee7a7c093139d35d1001e58db89e4a
KLaszlo24 <krajczlaci234@gmail.com>
2025-01-17 12:32:27 +0100

    Osszeadas_Kész

--- szamologep/Program.cs
+++ szamologep/Program.cs
@@ -8,8 +8,37 @@ namespace szamologep
 {
 	internal class Program
 	{
+
+		static float szam1 = 0;
+		static float szam2 = 0;
+		static string jel = "";
+
 		static void Main(string[] args)
 		{
+			Indit();
+
+		}
+		static void Indit()
+		{
+			Console.Write("Addj meg egy számot: ");
+			szam1 = float.Parse(Console.ReadLine());
+			Console.Write("Addj meg egy másik számot: ");
+			szam2 = float.Parse(Console.ReadLine());
+			Console.Write("Addj meg egy másik számot: ");
+			jel = Convert.ToString(Console.ReadLine());
+
+			switch (jel)
+			{
+				case "+":
+					Osszeadas();
+					break;
+			}
+		}
+
+		static void Osszeadas()
+		{
+			float ossz = szam1 + szam2;
+			Console.WriteLine($"Két szám összeadava: {ossz}");
 		}
 	}
 }
